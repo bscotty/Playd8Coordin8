6f58b51d02dc08dca0461ffe8911313c70acf2ed
Bryan Scott <bryan@pajato.com>
2016-12-11 12:03:58 -0500

    Fix for event creation -- we had a typo in one of our Firebase Fields, specifically "attending" was written with a colon which is just no good at all

--- playd8coordin8/EventMakerViewController.m
+++ playd8coordin8/EventMakerViewController.m
@@ -31,16 +31,12 @@
 // Creates an Event Object based on the UI Fields. These events do not have keys. Maybe move this to another ViewController Object where our events are generated?
 
 - (IBAction)createEvent:(id)sender {
-    NSString *time = _timeField.text;
-    NSString *date = _dateField.text;
-    NSString *location = _locationField.text;
-    NSString *guest = _guestField.text;
-
+    // Get the event object from the textfields.
     Event *e = [[Event alloc] init];
-    [e setTime:time];
-    [e setDate:date];
-    [e setLocation:location];
-    [e addGuest:guest];
+    [e setTime:_timeField.text];
+    [e setDate:_dateField.text];
+    [e setLocation:_locationField.text];
+    [e addGuest:_guestField.text];
     [e setIsAttending:@YES];
    
     // Puts the Event object onto Firebase.
@@ -49,25 +45,28 @@
     
     NSString *key = [events childByAutoId].key;
 
-    NSDictionary *post = @{@"attending:": e.isAttending,
-                                 @"time": e.time,
-                                 @"date": e.date,
-                             @"location": e.location,
-                               @"guests": e.guests};
+    NSDictionary *post = @{@"attending": e.isAttending,
+                                @"time": e.time,
+                                @"date": e.date,
+                            @"location": e.location,
+                              @"guests": e.guests};
     
     NSDictionary *childUpdates = @{[@"/events/" stringByAppendingString:key]: post};
     [ref updateChildValues:childUpdates];
     
+    // Reset the text fields.
     _timeField.text = @"Time";
     _dateField.text = @"Date";
     _locationField.text = @"Location";
     _guestField.text = @"Guest";
     
+    // Setup the alert, which creates a popup.
     UIAlertController* alert = [UIAlertController
-                                alertControllerWithTitle:@"My Alert"
+                                alertControllerWithTitle:@"Good News!"
                                                  message:@"Your event has been created."
                                           preferredStyle:UIAlertControllerStyleAlert];
     
+    // Setup the Action, which dismisses the viewcontroller when it's done.
     UIAlertAction* defaultAction =
                     [UIAlertAction actionWithTitle:@"OK"
                                              style:UIAlertActionStyleDefault
