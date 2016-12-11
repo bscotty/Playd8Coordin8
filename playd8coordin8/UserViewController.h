6b85cd4a3e39728919b6419d8e7409be08a07ae6
halinebr <hab310@nyu.edu>
2016-12-11 15:52:50 -0500

    Children table in the User Scene now loading with data

--- playd8coordin8/UserViewController.h
+++ playd8coordin8/UserViewController.h
@@ -7,12 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+@import Firebase;
 
-@interface UserViewController : UIViewController
+@interface UserViewController : UIViewController<UITableViewDelegate, UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UIImageView *userImage;
 
 @property (weak, nonatomic) IBOutlet UILabel *userName;
 
 @property (weak, nonatomic) IBOutlet UILabel *userEmail;
+@property (strong, nonatomic) FIRDatabaseReference *ref;
+@property (nonatomic) NSMutableArray *children;
+@property (weak, nonatomic) IBOutlet UITableView *childTableView;
 
 @end
--- playd8coordin8/UserViewController.m
+++ playd8coordin8/UserViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "UserViewController.h"
+#import "Child.h"
 @import Firebase;
 
 @interface UserViewController ()
@@ -17,6 +18,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.childTableView.delegate = self;
+    self.childTableView.dataSource = self;
     // Do any additional setup after loading the view.
     if([[FIRAuth auth] currentUser]) {
         FIRUser *user = [FIRAuth auth].currentUser;
@@ -31,8 +34,41 @@
         _userName.text = name;
         _userEmail.text = email;
         _userImage.image=[[UIImage alloc] initWithData:[NSData dataWithContentsOfURL:photoURL]];
-        
+       
     }
+    
+    FIRDatabaseReference *ref = [[FIRDatabase database] reference];
+    FIRUser *user = [FIRAuth auth].currentUser;
+    
+    FIRDatabaseReference *userData = [[ref child:@"users"] child:user.uid];
+    
+    NSLog(@"PD8 getting children of user");
+    
+    [userData observeEventType: FIRDataEventTypeValue withBlock:^(FIRDataSnapshot *_Nonnull snapshot){
+        NSLog(@"PD8 OBSERVING USERDATA DB");
+        self.children = [[NSMutableArray alloc] init];
+        for(FIRDataSnapshot* child in snapshot.children){
+            //create a new child
+            Child* c = [[Child alloc] init];
+            
+            //Get the Firebase Key, along with the age, type, bio, and name
+            [c setKey: [child key]];
+            [c setAge: [[child childSnapshotForPath:@"age"] value]];
+            [c setType: [[child childSnapshotForPath:@"type"] value]];
+            [c setBio: [[child childSnapshotForPath:@"bio"] value]];
+            [c setName: [[child childSnapshotForPath:@"name"] value]];
+            
+            NSLog(@"PD8 capturing child %@", c.name);
+            
+            [self.children addObject:c];
+        }
+        [self.childTableView reloadData];
+    } withCancelBlock:^(NSError * _Nonnull error){
+        NSLog(@"PD8 OBSERVE ERROR");
+        NSLog(@"%@", error.localizedDescription);
+    }];
+
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -41,6 +77,33 @@
 }
 
 #pragma mark - Table 
+#pragma mark - Table view data source
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSLog(@"PD8 TABLEVIEW ROWSINSECTION");
+    return [self.children count];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    NSLog(@"PD8 TABLEVIEW SECTIONSINVIEW");
+    return 1;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"PD8 TABLEVIEW CALLED");
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+    //Here the dataSource array is of dictionary objects
+    Child *child = self.children[indexPath.row];
+    cell.textLabel.text = child.name;
+    
+    return cell;
+}
+
+
 
 /*
 #pragma mark - Navigation
